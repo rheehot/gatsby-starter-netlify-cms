28a6fa5e4a7b64d90faf28d037faa8a326d945ad
JaesungPark <jspark141515#@gmail.com>
2020-03-13 17:43:29 +0900

    Update

--- src/components/CommonHr.js
+++ src/components/CommonHr.js
@@ -1,9 +1,12 @@
 import styled from "@emotion/styled";
 
 const CommonHr = styled.hr`
-  width: 72px;
+  width: 144px;
   border-width: 1px;
   margin-top: 24px;
+  margin-left: auto;
+  margin-right: auto;
+  color: #e1e4e8;
 `;
 
 export default CommonHr;
